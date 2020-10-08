904af08e8981266520d921afebd80efbcf48e0aa
JuniorTour <juniortour@qq.com>
2020-10-09 01:05:18 +0800

    add click flash tip

--- public/js/layers/wating.js
+++ public/js/layers/wating.js
@@ -1,4 +1,5 @@
 export function createWaitingLayer(font, logoImg) {
+    let showClickTip = 0
     return function drawDashboard(context) {
         context.drawImage(
             logoImg,
@@ -7,6 +8,13 @@ export function createWaitingLayer(font, logoImg) {
             200,
             90
         )
-        font.print('CLICK TO START', context, 76, 140);
+        if (showClickTip <= 60) {
+            font.print('CLICK TO START', context, 76, 140);
+            showClickTip++
+        } else if (showClickTip > 90) {
+            showClickTip = 0
+        } else if (showClickTip > 60) {
+            showClickTip++
+        }
     }
 }
